e9a6596ab84a7d8441e2f7f12bf15df140ebc560
wilsjw <wilsjw2@student.op.ac.nz>
2024-05-04 15:41:52 +1200

    feat: Created new Fabric mod

---
+++ src/main/java/com/ashures/LethalityClient.java
@@ -0,0 +1,10 @@
+package com.ashures;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LethalityClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
