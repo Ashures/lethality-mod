89014c4d93b19d8a38cdef067136ae633fa2f39f
wilsjw <wilsjw2@student.op.ac.nz>
2024-05-04 19:20:12 +1200

    fix: Reduced inflated Katana attack speed from 6.0 to 2.0

--- src/main/java/com/ashures/item/ModItems.java
+++ src/main/java/com/ashures/item/ModItems.java
@@ -13,7 +13,7 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item KATANA = registerItem("katana", new SwordItem(ToolMaterials.NETHERITE, 4, 2.0f, new FabricItemSettings()));
+    public static final Item KATANA = registerItem("katana", new SwordItem(ToolMaterials.NETHERITE, 4, - 2.0f, new FabricItemSettings()));
 
     // To add a different type of item (for example, Ingredient) create a new method for that new type.
     private static void addItemsToCombatItemGroup(FabricItemGroupEntries entries) {
