296cdc144f0cb6954a9535e20d1fa394f55a3f61
wilsjw <wilsjw2@student.op.ac.nz>
2024-05-04 16:15:53 +1200

    feat: Setup item adding requirements with tool item group example

--- src/main/java/com/ashures/Lethality.java
+++ src/main/java/com/ashures/Lethality.java
@@ -1,5 +1,6 @@
 package com.ashures;
 
+import com.ashures.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class Lethality implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
