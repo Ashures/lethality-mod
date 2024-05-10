b47c5c9809f6bf2319484a1838e96300addc3f1c
Ashures <ashuresyt@gmail.com>
2024-05-10 20:53:35 +1200

    feat: Added double jump ability when holding Assassin's Blade

---
+++ src/main/java/com/ashures/mixin/AssassinBladeDoubleJump.java
@@ -0,0 +1,56 @@
+package com.ashures.mixin;
+
+import com.ashures.item.ModItems;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.input.Input;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.recipebook.ClientRecipeBook;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.Items;
+import net.minecraft.stat.StatHandler;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public abstract class AssassinBladeDoubleJump extends AbstractClientPlayerEntity {
+
+    @Unique
+    private int jumpCooldown = 5;
+    @Shadow
+    public Input input;
+
+    @Shadow public abstract void sendMessage(Text message);
+
+    public AssassinBladeDoubleJump(ClientWorld world, GameProfile profile) {
+        super(world, profile);
+    }
+
+    @Inject(at = @At("HEAD"), method = "tickMovement")
+    private void tickMovement(CallbackInfo ci) {
+        if (this.isOnGround() || this.isClimbing()) {
+            jumpCooldown = 5;
+        } else {
+            jumpCooldown = Math.max(jumpCooldown - 1, 0);
+        }
+
+        if (
+            input.jumping && this.getMainHandStack().isOf(ModItems.ASSASSIN_BLADE) &&
+            !this.isOnGround() && !this.getAbilities().flying && !this.isFallFlying() && !this.hasVehicle() && jumpCooldown == 0
+        ) {
+            Vec3d curVelocity = this.getVelocity();
+            this.setVelocity(curVelocity.x, getJumpVelocity() * 1.5, curVelocity.z);
+
+            jumpCooldown = 20;
+        }
+    }
+}
