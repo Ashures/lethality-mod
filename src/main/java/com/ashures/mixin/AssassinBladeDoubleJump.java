cc93c7a2d0d196b50b3c38ecaedd1f25a5bc6387
Ashures <ashuresyt@gmail.com>
2024-05-11 19:51:43 +1200

    fix: Forced player to re-jump before double jump is used

--- src/main/java/com/ashures/mixin/AssassinBladeDoubleJump.java
+++ src/main/java/com/ashures/mixin/AssassinBladeDoubleJump.java
@@ -28,6 +28,8 @@ public abstract class AssassinBladeDoubleJump extends AbstractClientPlayerEntity
 
     @Unique
     private int jumpCooldown = 5;
+    @Unique
+    private boolean jumpedLastFrame = false;
     @Shadow
     public Input input;
 
@@ -47,15 +49,17 @@ public abstract class AssassinBladeDoubleJump extends AbstractClientPlayerEntity
             jumpCooldown = Math.max(jumpCooldown - 1, 0);
         }
 
-        if (input.jumping && this.getMainHandStack().isOf(ModItems.ASSASSIN_BLADE) &&
-            !this.isOnGround() && !this.getAbilities().flying && !this.isFallFlying() && !this.hasVehicle() && jumpCooldown == 0
+        if (input.jumping && this.getMainHandStack().isOf(ModItems.ASSASSIN_BLADE) && !jumpedLastFrame && jumpCooldown == 0
+            && !this.isOnGround() && !this.getAbilities().flying && !this.isFallFlying() && !this.hasVehicle()
         ) {
             Vec3d curVelocity = this.getVelocity();
-            this.setVelocity(curVelocity.x, getJumpVelocity() * 1.6F, curVelocity.z);
+            this.setVelocity(curVelocity.x, getJumpVelocity() * 2.0F, curVelocity.z);
 
             this.playSound(ModSounds.ASSASSIN_BLADE_DOUBLE_JUMP, 0.5F, 0.7F);
 
             jumpCooldown = 20;
         }
+
+        jumpedLastFrame = input.jumping;
     }
 }
