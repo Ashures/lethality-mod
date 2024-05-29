758fbb1fa2a68a471e500daa6cd2420e0de16496
Ashures <ashuresyt@gmail.com>
2024-05-29 23:08:53 +1200

    fix: Buffed attack speed on Dragon Lance

--- src/main/java/com/ashures/item/ModItems.java
+++ src/main/java/com/ashures/item/ModItems.java
@@ -15,7 +15,7 @@ public class ModItems {
     public static final Item KATANA = registerItem("katana", new SwordItem(ToolMaterials.NETHERITE, 2, -2.0f, new FabricItemSettings()));
     public static final Item ASSASSIN_BLADE = registerItem("assassin_blade", new AssassinBladeItem(ToolMaterials.NETHERITE, 2, -2.0f, 40, new FabricItemSettings()));
     public static final Item DRACONIC_RIFTSTEEL = registerItem("draconic_riftsteel", new Item(new FabricItemSettings()));
-    public static final Item DRAGON_LANCE = registerItem("dragon_lance", new DragonLanceItem(ToolMaterials.NETHERITE, 12, -3.7f, 200, new FabricItemSettings()));
+    public static final Item DRAGON_LANCE = registerItem("dragon_lance", new DragonLanceItem(ToolMaterials.NETHERITE, 12, -3.2f, 200, new FabricItemSettings()));
 
     // To add a different type of item (for example, Ingredient) create a new method for that new type.
     private static void addItemsToCombatItemGroup(FabricItemGroupEntries entries) {
